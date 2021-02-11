e81110e772c1c06aa7d7db0e6b34d621a459959d
JOY SINHA <51963863+sinhajoy@users.noreply.github.com>
2021-02-11 12:51:42 +0530

    Update

---
+++ PrimeGame.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 11 11:56:19 2021
+
+@author: joysi
+"""
+
+def mid(l,r):
+    return (l + (r - l) / 2)
+
+print(mid(1,10))
--- frogsort.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 10 22:42:08 2021
-
-@author: joysi
-"""
-
