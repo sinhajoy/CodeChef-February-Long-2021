428e10cad6e4941b37317ccd3a11b65875e3bcb4
Joy Sinha <joysinha8017@gmail.com>
2021-02-16 00:33:54 +0530

    Update manysums.py

--- manysums.py
+++ manysums.py
@@ -5,3 +5,14 @@ Created on Mon Feb 15 20:59:45 2021
 
 @author: user
 """
+
+s=int(input())
+for i in range(s):
+    a=input().split()
+    li=[]
+    while (int(a[0])!=int(a[-1])):
+        a[0]=a[0]+1
+        li.append(a[0])
+        
+        print(li)
+                
